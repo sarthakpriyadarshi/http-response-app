8ec5be194c77cc0b2f72f0324caadf31b222ee07
Sarthak Priyadarshi <sarthakpriyadarshi01@gmail.com>
2025-02-23 06:42:02 +0530

    Added localhost to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
 
         setLoading(true);
         try {
-            const res = await fetch("http://backend:8000/analyze", {
+            const res = await fetch("http://localhost:8000/analyze", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ url }),
